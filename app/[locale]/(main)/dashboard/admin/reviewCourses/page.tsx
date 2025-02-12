ec096abb955bbe76981ab9f39dbc403b8bc210e0
mahmoud hamed <m7modm42@gmail.com>
2025-02-12 13:49:06 +0300

    elete api's call from review page

--- app/[locale]/(main)/dashboard/admin/reviewCourses/page.tsx
+++ app/[locale]/(main)/dashboard/admin/reviewCourses/page.tsx
@@ -1,28 +1,8 @@
-import { API_ROUTES } from '@/app/api/apiRoutes';
-import { Course } from '@/app/interfaces/interfaces';
-import { authOptions } from '@/app/lib/nextAuth';
-import { getServerSession } from 'next-auth';
-import CourseList from '@/demo/components/CourseList';
 import { getTranslations } from 'next-intl/server';
+import ReviewCourseList from '@/demo/components/(admin)/ReviewCourseList';
 
 const AdminCoursesForReview = async () => {
     const t = await getTranslations('coursesForReview');
-
-    const session = await getServerSession(authOptions);
-    let courses: Course[] = [];
-
-    const res = await fetch(API_ROUTES.COURSES.GET_COURSES_FOR_REVIEW, {
-        headers: {
-            Authorization: `Bearer ${session?.accessToken}`
-        },
-        cache: 'no-store'
-    });
-
-    if (!res.ok) {
-        const error = await res.json();
-        throw new Error(error.message || 'Failed to fetch courses');
-    }
-    courses = await res.json();
 
     return (
         <div className="grid">
@@ -31,7 +11,7 @@ const AdminCoursesForReview = async () => {
                     <h5>{t('title')}</h5>
                     <p>{t('description')}</p>
                 </div>
-                <CourseList courses={courses} />
+                <ReviewCourseList />
             </div>
         </div>
     );
